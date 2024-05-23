47e30a0479c1fde3596973c17b566a204557f162
Matt984 <mattmazzek@gmail.com>
2024-05-23 16:05:55 +0200

    ds-bike-station: initial Commit

---
+++ module/main.py
@@ -0,0 +1,21 @@
+from nearest_distance import find_nearest_stations
+
+
+def main():
+    while True:
+        try:
+            K = int(input("Enter the number of nearest bike stations to find (K): "))
+            if K > 0:
+                break
+            else:
+                print("Please enter a positive integer.")
+        except ValueError:
+            print("Invalid input. Please enter a positive integer.")
+
+    current_location = (34.0522, -118.2437)
+    print(f"Your Location: {current_location}")
+    find_nearest_stations(K, current_location)
+
+
+if __name__ == "__main__":
+    main()
